bd7b2ac94c7505325e76b36d7bf9733d46f2d6aa
Ash-5p <ashleyjph@gmail.com>
2025-04-26 23:05:01 +0100

    Add product, category, subcategory & brand models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Product, Category, Subcategory, Brand
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'subcategory', 'brand', 'price')
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "subcategory":
+            if request._obj_ is not None:
+                kwargs["queryset"] = request._obj_.category.subcategory.all()
+            else:
+                kwargs["queryset"] = Subcategory.objects.none()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_form(self, request, obj=None, **kwargs):
+        request._obj_ = obj
+        return super().get_form(request, obj, **kwargs)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('friendly_name', )
+    search_fields = ['name', 'friendly_name']
+    filter_horizontal = ('subcategory',)
+
+
+admin.site.register(Subcategory)
+admin.site.register(Brand)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    subcategory = models.ManyToManyField(
+        'Subcategory', blank=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    subcategory = models.ForeignKey('Subcategory', null=True, blank=True, on_delete=models.SET_NULL)
+    brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.name
