db673cbbfbaeb43d247b326f264c362c0ab67364
Ash-5p <ashleyjph@gmail.com>
2025-05-03 17:48:01 +0100

    Create checkout app & model

--- main/settings.py
+++ main/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -176,5 +177,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-FREE_DELIVERY_THRESHOLD = 50
+FREE_DELIVERY_THRESHOLD = 99
 STANDARD_DELIVERY_PERCENTAGE = 10
